1b2692efd12e6b0a5629a723383f668ed7ad83d8
lanqiukun <1543323033@qq.com>
2025-12-01 10:27:04 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wxauto',
+    version='3.9.11.17',  # 可以自定义版本号
+    description='A fork of wxauto for WeChat automation',
+    packages=find_packages(), # 这会自动找到wxauto目录
+)
